1ccd9efe3bf406df4d5c92247c92ea6015abc490
mrcbq <marcobaquero@gmail.com>
2023-09-21 18:10:16 -0500

    Fix linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :reservations, dependant: :destroy
 
-  validates :name, presence: true, length: { maximum: 100 }  
+  validates :name, presence: true, length: { maximum: 100 }
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,8 +1,7 @@
 class Vehicle < ApplicationRecord
   has_many :reservations, dependant: :destroy
 
-  validates :description, presence: true, length: { maximum: 10000 }
+  validates :description, presence: true, length: { maximum: 10_000 }
   validates :price, presence: true
   validates :photo, presence: true
-
 end
