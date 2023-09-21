3732fc5a917152d87affe66724c0199855b6d5d4
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-21 14:36:02 -0300

    Setup hello world

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
-import '@hotwired/turbo-rails';
-import './controllers';
+// import '@hotwired/turbo-rails';
+// import './controllers';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <h1>Hello world!!</h1>
+);
