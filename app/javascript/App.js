9fd65ccefaee2de25f86bb66359a5da3162c637d
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-21 17:38:03 -0400

    Update App component with Home route

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+
 function App() {
   return (
-    <h1> App component </h1>
-  );
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
