687a01cd95d14a076f5f06bc9f6a670bf9aee09e
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-21 19:06:15 -0300

    Add routes between components

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './routes/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from ""
+import Home from "./routes/Home";
+import Vehicles from "./routes/Vehicles";
+import Details from "./routes/Details";
+// import Vehicle from "./routes/Vehicle";
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="vehicles" element={<Vehicles />} />
+          <Route path="details" element={<Details />} />
+        </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
---
+++ app/javascript/routes/Details.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import VehicleDetails from '../components/vehicles/VehicleDetails'
+
+const Details = () => (
+    <>
+        <main>
+            <VehicleDetails />
+        </main>
+    </>
+)
+
+export default Details
