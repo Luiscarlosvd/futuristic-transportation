24213b17bb518a5822476ad61d241667aff15e05
mrcbq <marcobaquero@gmail.com>
2023-09-22 17:59:25 -0500

    Update User model to anulate the password requirement, Add verify_authenticity_token in user controller, test methods index n create
    Co-authored-by: enmanuelbayen <enmanuel0495@gmail.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: %i[show destroy]
+  # before_action :set_user, only: %i[show destroy]
+  skip_before_action :verify_authenticity_token, only: %i[create destroy]
 
-  # GET /api/v1/users
   def index
     @users = User.all
     if @users.present?
@@ -13,17 +13,11 @@ class Api::V1::UsersController < ApplicationController
     render json: { success: false, message: e.message }
   end
 
-  # GET /api/v1/users/1
-  def show
-    render json: @user
-  end
-
   # POST /api/v1/users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: { success: true, user: @user }
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -36,13 +30,11 @@ class Api::V1::UsersController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email)
   end
 end
