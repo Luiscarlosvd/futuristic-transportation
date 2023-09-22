7050958feb9f3688e9a30d52e98cd07f26b2f144
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-22 18:50:35 -0300

    Add signup structure and some styles

--- app/javascript/components/authentication/SignupForm.js
+++ app/javascript/components/authentication/SignupForm.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const SignupForm = () => (
-  <div>
-    <h1>Signup</h1>
+  <div className='w-full h-screen bg-slate-600'>
+    <div className='bg-image-form w-full h-screen grid place-content-center'>
+      <div className='form-bg px-32 py-16 flex flex-col gap-20 items-center rounded-lg'>
+        <h1 className='font-ace text-2xl text-white'>Sign Up</h1>
+        <form className='flex flex-col gap-7 items-center'>
+          <input type='text' placeholder='Username' className='font-ace border-white rounded-full'/>
+          <button type='submit' className='bg-white font-ace text-slate-600 py-1 px-4 rounded-full'> Register </button>
+        </form>
+      </div>
+    </div>
   </div>
 );
 
