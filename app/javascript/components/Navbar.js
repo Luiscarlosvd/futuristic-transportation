63039e5108758419ee12ede6e9fffcb68ab5d452
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-22 14:54:39 -0300

    Add home section title, bg-color and image

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>
+  <div className='fixed bg-slate-600'>
     <h1>Navbar</h1>
     <NavLink to="/">Home</NavLink>
     <NavLink to="/vehicles">Vehicles List</NavLink>
--- app/javascript/components/PrincipalMain.js
+++ app/javascript/components/PrincipalMain.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const PrincipalMain = () => (
-  <div>
-    <h1>Principal Main Page</h1>
+  <div className='w-full h-screen flex items-start justify-center bg-secondaryOrange'>
+    <div className='w-full h-screen flex items-start justify-center home-img'>
+      <div className='container  mt-24'>
+        <h1 className='font-ace text-5xl text-white text-center'>Futuristic Gears</h1>
+      </div>
+    </div>
   </div>
 );
 
