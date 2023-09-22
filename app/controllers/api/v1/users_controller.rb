24d35fb1adaf1b2c912df6f48b7278a47225d0b9
enmanuelbayen <enmanuel0495@gmail.com>
2023-09-22 14:43:02 -0400

    add devise gem and remove method in model/user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::UsersController < ApplicationController
 
-    def index
-        @users = User.all
-    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
   has_many :reservations, dependant: :destroy
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
-
-  def self.authenticate(name)
-    user = find_by(name: name)
-    return user if user
-    nil # Return nil if no user is found
-  end
+  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }  
 end
