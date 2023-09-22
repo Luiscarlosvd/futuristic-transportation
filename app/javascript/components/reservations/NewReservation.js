6b3035de13a3f5f95ae3cdf98c1cd35b1241a093
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-22 10:04:05 -0400

    Create NewReservation component and Route

---
+++ app/javascript/components/reservations/NewReservation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewReservation = () => {
+  return (
+    <div>
+      <h1>New Reservation form</h1>
+    </div>
+  )
+}
+
+export default NewReservation
---
+++ app/javascript/routes/NewReserve.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewReservation from '../components/reservations/NewReservation'
+
+const NewReserve = () => {
+  return (
+    <div>
+      <NewReservation />
+    </div>
+  )
+}
+
+export default NewReserve
