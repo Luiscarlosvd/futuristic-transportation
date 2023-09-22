bff2fc0007a177b24666c8cacfe9e64d4144c22e
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-22 11:17:45 -0400

    Add routes to add and delete vehicles

---
+++ app/javascript/routes/DeleteVehicle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DeleteVehicleList from '../components/vehicles/DeleteVehicleList'
+
+const DeleteVehicle = () => {
+  return (
+    <div>
+      <DeleteVehicleList />
+    </div>
+  )
+}
+
+export default DeleteVehicle
---
+++ app/javascript/routes/NewVehicle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddVehicle from '../components/vehicles/AddVehicle'
+
+const NewVehicle = () => {
+  return (
+    <div>
+      <AddVehicle />
+    </div>
+  )
+}
+
+export default NewVehicle
