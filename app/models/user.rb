03566f4161843544ffa71b018ac64408a2460943
enmanuelbayen <enmanuel0495@gmail.com>
2023-09-22 14:52:39 -0400

    update routes.rb, user model and controller/application to api version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :reservations, dependant: :destroy
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'root#index'
+
+  devise_for :users
   namespace :api do
     namespace :v1 do
       resources :users, except: [:update]
