9a948537e893b656d6d84d0c52db0ded747e7627
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-22 11:16:34 -0400

    Add components to add and delete vehicles

---
+++ app/javascript/components/vehicles/AddVehicle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddVehicle = () => {
+  return (
+    <div>
+      <h1>Add a new Vehicle Form</h1>
+    </div>
+  )
+}
+
+export default AddVehicle
---
+++ app/javascript/components/vehicles/DeleteVehicleList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteVehicleList = () => {
+  return (
+    <div>
+      <h1>Vehicles List to delete</h1>
+    </div>
+  )
+}
+
+export default DeleteVehicleList
