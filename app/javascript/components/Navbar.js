f03ebe45beb625481728705b5d48182543349cc2
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-25 17:51:27 -0300

    Add media queries and conditions on the navbar

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
         }}
         className={`fixed text-primaryGreen left-3 top-3  text-3xl cursor-pointer nav-icon ${
           show ? "display" : "hide"
-        }`}
+        } ${location.pathname === "/" || location.pathname === "/log-in" || location.pathname === "/sign-up"  ? "block" : "hidden" }`}
       />
       <div
         className={`bg-white fixed h-screen flex flex-col justify-start z-50 w-60 nav-container ${
@@ -34,76 +34,46 @@ const Navbar = () => {
         }`}
       >
         <img src={logo} className="w-2/3 mx-auto" />
-        <div className="flex flex-col font-ace ml-2 text-slate-800 font-bold navigation">
-          <NavLink className="px-2 py-1" to="/">
+        <div className="flex flex-col font-ace ml-2 text-darkGrey font-bold navigation">
+          <NavLink className="p-2" to="/">
             HOME
           </NavLink>
-          <NavLink className="px-2 py-1" to="/vehicles">
+          <NavLink className="p-2" to="/vehicles">
             VEHICLES LIST
           </NavLink>
-          <NavLink className="px-2 py-1" to="/details/id">
+          <NavLink className="p-2" to="/details/id">
             VEHICLE DETAILS
           </NavLink>
-          <NavLink className="px-2 py-1" to="/my-reservations">
+          <NavLink className="p-2" to="/my-reservations">
             MY RESERVATIONS
           </NavLink>
-          <NavLink className="px-2 py-1" to="/reserve">
+          <NavLink className="p-2" to="/reserve">
             RESERVE
           </NavLink>
-          <NavLink className="px-2 py-1" to="details/id/reserve">
+          <NavLink className="p-2" to="details/id/reserve">
             RESERVE SPECIFIC
           </NavLink>
-          <NavLink className="px-2 py-1" to="log-in">
+          <NavLink className="p-2" to="log-in">
             LOGIN{" "}
           </NavLink>
-          <NavLink className="px-2 py-1" to="sign-up">
+          <NavLink className="p-2" to="sign-up">
             SIGNUP
           </NavLink>
-          <NavLink className="px-2 py-1" to="vehicles/new">
+          <NavLink className="p-2" to="vehicles/new">
             ADD VEHICLE
           </NavLink>
           <NavLink className="px-2 py-1" to="vehicles/delete">
             DELETE VEHICLE
           </NavLink>
         </div>
-        <div className="mt-auto">MEDIA</div>
+        <div className="mt-auto">
+          <div>
+            
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
 export default Navbar;
-
-// import React from 'react';
-// import { useLocation } from 'react-router-dom';
-
-// function Navbar() {
-//   // Get the current location object
-//   const location = useLocation();
-
-//   // Conditionally render different components based on the pathname
-//   return (
-//     <nav>
-//       <ul>
-//         <li>
-//           <a href="/">Home</a>
-//         </li>
-//         <li>
-//           <a href="/about">About</a>
-//         </li>
-//         {location.pathname === '/' && (
-//           <li>
-//             <a href="/contact">Contact</a>
-//           </li>
-//         )}
-//         {location.pathname === '/about' && (
-//           <li>
-//             <a href="/">Back</a>
-//           </li>
-//         )}
-//       </ul>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
