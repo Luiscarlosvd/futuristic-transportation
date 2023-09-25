6430b49c89c98313727013676e10ce8ec17ded02
mrcbq <marcobaquero@gmail.com>
2023-09-25 12:10:26 -0500

    Add logic to destroy reservation methods
    Co-authored-by: enmanuelbayen <enmanuel0495@gmail.com>

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,18 +2,25 @@ class Api::V1::ReservationsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: %i[create destroy]
 
   def index
-    @reservations = Reservations.all
+    @reservations = Reservation.all
     render json: @reservations
   end
 
   def create
-    @reservations = Reservations.new(reservations_params)
-    if @reservations.save
-      render json: @reservations, status: :created
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      render json: @reservation, status: :created
     else
-      render json: @reservations.errors, status: :unprocessable_entity
+      render json: @reservation.errors, status: :unprocessable_entity
     end
   end
 
-  def destroy; end
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    if @reservation.destroy
+      render json: { message: 'Vehicle deleted successfully' }, status: :no_content
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
