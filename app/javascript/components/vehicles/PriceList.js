3b6fba7f44c4d38c0a80e1bbf2e3a75f58fa3b48
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-25 17:25:49 -0400

    Create priceList component for vehicle details

---
+++ app/javascript/components/vehicles/PriceList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const PriceList = ({ price }) => {
+  const financeFee = (price) => {
+    return (price / 48).toFixed(2)
+  };
+    
+  const reserveForDay = (price) => {
+    return (price / 365).toFixed(2)
+  };
+  
+  return (
+    <div className='my-6 font-roboto text-lg shadow-md'>
+        <div className='price-list'>
+            <p>Finance fee</p>
+            <p className='font-bold'>${financeFee(price)}</p>
+        </div>
+        <div className='price-list'>
+            <p>Total amount payable</p>
+            <p className='font-bold'>${price}</p>
+        </div>
+        <div className='price-list'>
+            <p>Duration</p>
+            <p className='font-bold'>48 Months</p>
+        </div>
+        <div className='price-list'>
+            <p>Reserve for a day</p>
+            <p className='font-bold'>${reserveForDay(price)}</p>
+        </div>
+    </div>
+  );
+};
+
+export default PriceList
