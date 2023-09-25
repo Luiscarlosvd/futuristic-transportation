e938d4fe2019d09aee3adc30a2d1e6f9a0d606a4
mrcbq <marcobaquero@gmail.com>
2023-09-25 12:36:03 -0500

    Initialize Users using migration to probe the vehicles API
    Co-authored-by: enmanuelbayen <enmanuel0495@gmail.com>

--- db/migrate/20230925171706_initialize_vehicles.rb
+++ db/migrate/20230925171706_initialize_vehicles.rb
@@ -1,8 +1,8 @@
-class InitializeGreetings < ActiveRecord::Migration[7.0]
+class InitializeVehicles < ActiveRecord::Migration[7.0]
   def up
     Vehicle.create(
       name: "Toyota Corolla",
-      description: "Un confiable y eficiente vehículo compacto.",
+      description: "Un confiable y eficiente compacto.",
       price: 20000.00,
       photo: "https://example.com/images/toyota_corolla_front.jpg",
       photo_back: "https://example.com/images/toyota_corolla_back.jpg",
@@ -12,7 +12,7 @@ class InitializeGreetings < ActiveRecord::Migration[7.0]
     
     Vehicle.create(
       name: "Honda Civic",
-      description: "Un vehículo compacto con un rendimiento deportivo.",
+      description: "Un compacto con un rendimiento deportivo.",
       price: 22000.00,
       photo: "https://example.com/images/honda_civic_front.jpg",
       photo_back: "https://example.com/images/honda_civic_back.jpg",
@@ -22,7 +22,7 @@ class InitializeGreetings < ActiveRecord::Migration[7.0]
     
     Vehicle.create(
       name: "Ford F-150",
-      description: "Una camioneta robusta y versátil.",
+      description: "Una camioneta robusta.",
       price: 30000.00,
       photo: "https://example.com/images/ford_f150_front.jpg",
       photo_back: "https://example.com/images/ford_f150_back.jpg",
