f4a50f6c9625f2958f006a91b611fdfc1552e353
mrcbq <marcobaquero@gmail.com>
2023-09-25 12:07:50 -0500

    Add logic to index and create reservations methods
    Co-authored-by: enmanuelbayen <enmanuel0495@gmail.com>

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::ReservationsController < ApplicationController
-  def index; end
+  skip_before_action :verify_authenticity_token, only: %i[create destroy]
 
-  def show; end
+  def index
+    @reservations = Reservations.all
+    render json: @reservations
+  end
 
-  def create; end
+  def create
+    @reservations = Reservations.new(reservations_params)
+    if @reservations.save
+      render json: @reservations, status: :created
+    else
+      render json: @reservations.errors, status: :unprocessable_entity
+    end
+  end
 
   def destroy; end
 end
