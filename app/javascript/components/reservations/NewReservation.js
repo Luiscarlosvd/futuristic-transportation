9d20c3b55468860287880e7aa984579999a879d5
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-26 19:46:38 -0400

    Fix linters

--- app/javascript/components/reservations/NewReservation.js
+++ app/javascript/components/reservations/NewReservation.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect } from "react";
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useLocation } from 'react-router-dom';
 import { PongSpinner } from 'react-spinners-kit';
@@ -13,65 +12,80 @@ const NewReservation = () => {
   const vehicle = useSelector((state) => state.vehicles);
 
   useEffect(() => {
-    if (vehicle.vehicles.length === 0 ) {
+    if (vehicle.vehicles.length === 0) {
       dispatch(getVehiclesInfo());
     }
   }, [dispatch]);
 
   return (
     <>
-      {vehicle.status === 'Loading'  && <div className="h-screen flex justify-center items-center"><PongSpinner size={100} color="#686769" loading /></div> }
+      {vehicle.status === 'Loading' && <div className="h-screen flex justify-center items-center"><PongSpinner size={100} color="#686769" loading /></div> }
       {vehicle.status === 'fulfilled' && (
         <>
-          <div className='h-full w-full bg-new-reservation'>
+          <div className="h-full w-full bg-new-reservation">
             <div className="bg-image-new-reservation w-full h-full grid place-content-center">
-              <section className='form-new-reservation-bg flex flex-col justify-center items-center sm:max-w-5xl sm:w-4/5 lg:px-6'>
-                <h1 className='font-ace text-2xl text-white text-shadow-title text-center'>Make a Reservation</h1>
-                <div className='bg-white h-1px w-5/6 max-w-xs my-5'></div>
-                <p className='text-white text-center w-11/12 text-shadow-title'>
-                  In our catalog we have availability of 20 vehicles and the ability 
-                  to create any vehicle that you imagine completely tailored to your needs 
-                  if you wish to purchase it. The future is today and we can create whatever 
-                  your mind imagines. If you want to find out if a reservation is available in your area, 
+              <section className="form-new-reservation-bg flex flex-col justify-center items-center sm:max-w-5xl sm:w-4/5 lg:px-6">
+                <h1 className="font-ace text-2xl text-white text-shadow-title text-center">Make a Reservation</h1>
+                <div className="bg-white h-1px w-5/6 max-w-xs my-5" />
+                <p className="text-white text-center w-11/12 text-shadow-title">
+                  In our company we have the ability
+                  to create any vehicle that you imagine completely tailored to your needs
+                  if you wish to purchase it. The future is today and we can create whatever
+                  your mind imagines. If you want to find out if a
+                  reservation is available in your area,
                   please use the selector below.
                 </p>
-                <form className="flex flex-col gap-7 items-center mt-5 w-full lg:flex-row" >
-                  <select type="text" placeholder="City" className="font-roboto shadow-md border-white rounded-full bg-transparent text-white py-4 w-11/12" >
-                    <option className="bg-zinc-800" value="" disabled > Select a City </option>
-                    <option className='bg-zinc-800' value="Cordoba" > Cordoba, Argentina </option>
-                    <option className='bg-zinc-800' value="Barquisimeto" > Barquisimeto, Venezuela </option>
-                    <option className='bg-zinc-800' value="Carabobo" > Carabobo, Venezuela </option>
-                    <option className='bg-zinc-800' value= "Medellin" > Medellin, Colombia </option>
+                <form className="flex flex-col gap-7 items-center mt-5 w-full lg:flex-row">
+                  <select type="text" placeholder="City" className="font-roboto shadow-md border-white rounded-full bg-transparent text-white py-4 w-11/12">
+                    <option className="bg-zinc-800" value="" disabled> Select a City </option>
+                    <option className="bg-zinc-800" value="Cordoba"> Cordoba, Argentina </option>
+                    <option className="bg-zinc-800" value="Barquisimeto"> Barquisimeto, Venezuela </option>
+                    <option className="bg-zinc-800" value="Carabobo"> Carabobo, Venezuela </option>
+                    <option className="bg-zinc-800" value="Medellin"> Medellin, Colombia </option>
                   </select>
                   <div className="w-11/12 flex justify-start relative">
                     <input
-                      required={true}
+                      required
                       type="date"
                       name="date"
                       placeholder="Date"
-                      min={new Date().toISOString().split("T")[0]}
+                      min={new Date().toISOString().split('T')[0]}
                       className="w-full font-roboto shadow-md border-white rounded-full bg-transparent text-white input-date py-4"
                     />
                   </div>
-                    { location.pathname === '/reserve' ? (
-                        <select type="text" placeholder="Vehicle" className="font-roboto shadow-md border-white rounded-full bg-transparent text-white py-4 w-11/12" >
-                          <option className="bg-zinc-800" value="" disabled > Select a Vehicle </option>
-                          {vehicle.vehicles.map((vehicle) => (
-                            <option key={vehicle.id} value={vehicle.id} className="bg-zinc-800"> {vehicle.name} </option>
-                          ))}
-                        </select>
-                      ) : (
-                        <select type="text" placeholder="Vehicle" className="font-roboto shadow-md border-white rounded-full bg-transparent text-white py-4 w-11/12">
-                          {vehicle.vehicles.map((vehicle) => {
-                            if (vehicle.id === parseInt(vehicleId, 10)) {
-                              return <option key={vehicle.id} value={vehicle.id} className="bg-zinc-800" selected> {vehicle.name} </option>
-                            }
-                          })}
-                        </select>
-                      ) }
-                  <button type="submit" className="font-roboto font-medium w-4/5 max-w-xs transition-scale text-primaryGreen
-                  bg-white border-white py-4 px-8 rounded-full md:w-60"> 
-                    Reserve 
+                  { location.pathname === '/reserve' ? (
+                    <select type="text" placeholder="Vehicle" className="font-roboto shadow-md border-white rounded-full bg-transparent text-white py-4 w-11/12">
+                      <option className="bg-zinc-800" value="" disabled> Select a Vehicle </option>
+                      {vehicle.vehicles.map((vehicle) => (
+                        <option key={vehicle.id} value={vehicle.id} className="bg-zinc-800">
+                          {' '}
+                          {vehicle.name}
+                          {' '}
+                        </option>
+                      ))}
+                    </select>
+                  ) : (
+                    <select type="text" placeholder="Vehicle" className="font-roboto shadow-md border-white rounded-full bg-transparent text-white py-4 w-11/12">
+                      {vehicle.vehicles.map((vehicle) => {
+                        if (vehicle.id === parseInt(vehicleId, 10)) {
+                          return (
+                            <option key={vehicle.id} value={vehicle.id} className="bg-zinc-800" selected>
+                              {' '}
+                              {vehicle.name}
+                              {' '}
+                            </option>
+                          );
+                        }
+                        return null;
+                      })}
+                    </select>
+                  ) }
+                  <button
+                    type="submit"
+                    className="font-roboto font-medium w-4/5 max-w-xs transition-scale text-primaryGreen
+                  bg-white border-white py-4 px-8 rounded-full md:w-60"
+                  >
+                    Reserve
                   </button>
                 </form>
               </section>
