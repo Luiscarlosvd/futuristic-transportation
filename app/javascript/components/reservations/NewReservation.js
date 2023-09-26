df08dc73212edd9583d8b871865ad60db077d704
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-26 12:47:28 -0400

    Add title and background image

--- app/javascript/components/reservations/NewReservation.js
+++ app/javascript/components/reservations/NewReservation.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const NewReservation = () => (
-  <div>
-    <h1>New Reservation form</h1>
-  </div>
-);
+const NewReservation = () => {
+  return (
+    <div className='h-screen w-full bg-new-reservation'>
+      <div className="bg-image-new-reservation w-full h-screen grid place-content-center">
+        <section className='flex flex-col justify-center items-center'>
+          <h1 className='font-ace text-2xl text-white text-shadow-title'>Make a Reservation</h1>
+          <div className='bg-white h-1px w-5/6 max-w-xs'></div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default NewReservation;
