a642a354a293f34920f5db1450758415ff2a4320
mrcbq <marcobaquero@gmail.com>
2023-09-26 11:27:59 -0500

    Add logic to reservationReducer to handle reservations API

--- app/javascript/redux/reservationsSlice.js
+++ app/javascript/redux/reservationsSlice.js
@@ -1,7 +1,13 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getReservationsInfo = createAsyncThunk('reservations/getReservationsInfo',
+const initialState = {
+  reservations: [],
+  status: 'idle',
+  error: null,
+};
+
+export const fetchReservations = createAsyncThunk('reservations/fetchReservations',
   async () => {
     try {
       const response = await axios.get('api/v1/reservations');
@@ -11,43 +17,16 @@ export const getReservationsInfo = createAsyncThunk('reservations/getReservation
     }
   });
 
-const initialState = {
-  vehicles: [
-    {
-      id: 1,
-      name: 'Super duper vehicle',
-      description: 'This is the best vehicle you are going to see',
-      price: 20000.99,
-      photo: 'https://lh3.googleusercontent.com/u/2/drive-viewer/AK7aPaBZu9r6FDBzFsC-K13-wIdpBV3ncfnI35pQJfcRzpkOO0MOq_16QER_pHSAf7RZvXnTZYJ_SnasIVS8mUjoq06_-bWpbQ=w1868-h903',
-      photo_back: 'link.png',
-      photo_left: 'link.png',
-      photo_right: 'link.png',
-    },
-    {
-      id: 2,
-      name: 'Super duper vehicle number 2',
-      description: 'This is the best vehicle you are going to see x2',
-      price: 39.99,
-      photo: 'link.png',
-      photo_back: 'link.png',
-      photo_left: 'link.png',
-      photo_right: 'link.png',
-    },
-  ],
-  status: 'idle',
-  error: null,
-};
-
-const vehiclesSlice = createSlice({
-  name: 'vehicles',
+const reservationsSlice = createSlice({
+  name: 'reservations',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(getVehiclesInfo.fulfilled, (state, action) => ({ ...state, status: 'fulfilled', vehicles: action.payload }))
-      .addCase(getVehiclesInfo.pending, (state) => ({ ...state, status: 'Loading' }))
-      .addCase(getVehiclesInfo.rejected, (state, action) => ({ ...state, status: 'rejected', error: action.error.message }));
+      .addCase(fetchReservations.pending, (state) => ({ ...state, status: 'pending' }))
+      .addCase(fetchReservations.fulfilled, (state, action) => ({ ...state, status: 'fulfilled', reservations: action.payload }))
+      .addCase(fetchReservations.rejected, (state, action) => ({ ...state, status: 'rejected', error: action.error.message }));
   },
 });
 
-export default vehiclesSlice.reducer;
+export default reservationsSlice.reducer;
