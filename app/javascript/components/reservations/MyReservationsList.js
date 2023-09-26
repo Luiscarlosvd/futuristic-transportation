2e1641b5ca7465e13202c03aaf29d0897a903b1d
mrcbq <marcobaquero@gmail.com>
2023-09-26 17:36:16 -0500

    Delete initial state to use the fetch API

--- app/javascript/components/reservations/MyReservationsList.js
+++ app/javascript/components/reservations/MyReservationsList.js
@@ -1,30 +1,55 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchReservations } from '../../redux/reservationsSlice';
+import { getVehiclesInfo } from '../../redux/vehicleSlice';
 
 const MyReservationsList = () => {
   const dispatch = useDispatch();
-  const { reservations, status, error } = useSelector(
-    (store) => store.reservations
-  );
+  const { reservations } = useSelector((store) => store.reservations);
+  const { vehicles } = useSelector((store) => store.vehicles);
+
+  const userId = 1;
 
   useEffect(() => {
     if (reservations.length === 0) {
       dispatch(fetchReservations());
     }
+    if (vehicles.length === 0) {
+      dispatch(getVehiclesInfo());
+    }
   }, []);
 
-  console.log(reservations, status, error);
+  const userReserves = reservations.filter((reservation) => reservation.user_id === userId);
+  const vehicleIds = userReserves.map((reservation) => reservation.vehicle_id);
+  const filteredVehicles = vehicles.filter((vehicle) => vehicleIds.includes(vehicle.id));
+
+  console.log(userReserves, vehicleIds, filteredVehicles);
 
   return (
     <div className="flex">
       <div className="navbar-space" />
       <div className="min-h-screen">
-        {reservations.map((reservation) => (
-          <div key={reservation.id} className="reservationContainer">
-            <h2>{reservation.city}</h2>
-          </div>
-        ))}
+
+        <h1 className="text-center text-xl font-bold text-gray-700 py-6">Reservations</h1>
+        <div className="flex flex-row flex-wrap justify-center gap-8">
+
+          {filteredVehicles.map((vehicle, index) => (
+            <div key={vehicle.id} className="flex flex-col items-center">
+              <img
+                src={vehicle.photo}
+                alt={vehicle.name}
+                className="w-64 h-40 object-cover"
+              />
+              <div className="bg-white w-64 rounded-xl shadow-md p-4">
+                <p className="text-gray-700 font-bold text-lg">{vehicle.name}</p>
+                <p className="text-gray-500 text-sm">{vehicle.description}</p>
+                <p className="text-gray-500 font-bold text-sm">{userReserves[index].city}</p>
+                <p className="text-gray-500 text-sm">{userReserves[index].event_date}</p>
+              </div>
+            </div>
+          ))}
+
+        </div>
       </div>
     </div>
   );
