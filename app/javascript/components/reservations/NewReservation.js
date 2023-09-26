9984bb7dd485a7d6c27b994841ad4ccad6d66b5a
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-26 17:49:03 -0400

    Create date input at new reservation form

--- app/javascript/components/reservations/NewReservation.js
+++ app/javascript/components/reservations/NewReservation.js
@@ -12,15 +12,13 @@ const NewReservation = () => {
     dispatch(getVehiclesInfo());
   }, [dispatch]);
 
-  console.log(vehicle)
-
   return (
     <>
       {vehicle.status === 'Loading' && <div className="h-screen flex justify-center items-center"><PongSpinner size={100} color="#686769" loading /></div> }
       {vehicle.status === 'fulfilled' && (
         <>
-          <div className='h-screen w-full bg-new-reservation'>
-            <div className="bg-image-new-reservation w-full h-screen grid place-content-center">
+          <div className='h-full w-full bg-new-reservation'>
+            <div className="bg-image-new-reservation w-full h-full grid place-content-center">
               <section className='form-new-reservation-bg flex flex-col justify-center items-center'>
                 <h1 className='font-ace text-2xl text-white text-shadow-title text-center'>Make a Reservation</h1>
                 <div className='bg-white h-1px w-5/6 max-w-xs my-5'></div>
@@ -33,14 +31,24 @@ const NewReservation = () => {
                 </p>
                 <form className="flex flex-col gap-7 items-center mt-5 w-full">
                   <select type="text" placeholder="City" className="font-roboto shadow-md border-white rounded-full bg-transparent text-white py-4 w-11/12" >
-                    <option className="bg-zinc-800" value="" disabled selected > Select a City </option>
+                    <option className="bg-zinc-800" value="" disabled > Select a City </option>
                     <option className='bg-zinc-800' value="Cordoba" > Cordoba, Argentina </option>
                     <option className='bg-zinc-800' value="Barquisimeto" > Barquisimeto, Venezuela </option>
                     <option className='bg-zinc-800' value="Carabobo" > Carabobo, Venezuela </option>
                     <option className='bg-zinc-800' value= "Medellin" > Medellin, Colombia </option>
                   </select>
+                  <div className="w-11/12 flex justify-start relative">
+                    <input
+                      required={true}
+                      type="date"
+                      name="date"
+                      placeholder="Date"
+                      min={new Date().toISOString().split("T")[0]}
+                      className="w-full font-roboto shadow-md border-white rounded-full bg-transparent text-white input-date"
+                    />
+                  </div>
                   <select type="text" placeholder="Vehicle" className="font-roboto shadow-md border-white rounded-full bg-transparent text-white py-4 w-11/12" >
-                    <option className="bg-zinc-800" value="" disabled selected > Select a Vehicle </option>
+                    <option className="bg-zinc-800" value="" disabled > Select a Vehicle </option>
                     {vehicle.vehicles.map((vehicle) => (
                       <option key={vehicle.id} value={vehicle.id} className="bg-zinc-800"> {vehicle.name} </option>
                     ))}
