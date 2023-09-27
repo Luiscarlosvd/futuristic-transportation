b41743a05b3a16c35597e2898f4aa512cc30b456
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-27 12:28:10 -0300

    Add final VehicleCard styles and structure

--- app/javascript/components/vehicles/VehicleCard.js
+++ app/javascript/components/vehicles/VehicleCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BsTwitter } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+import { AiOutlineInstagram } from 'react-icons/ai'
+import Divisor from './Divisor';
 
-const VehicleCard = ( { name, description, image } ) => (
-  <div className='flex flex-col items-center justify-center gap-3'>
-    <p>{image}</p>
-    <h3 className='font-ace'>{name}</h3>
-    <p>{description}</p>
-    <p>media</p>
-
-  </div>
+const VehicleCard = ( { id, name, description, price, photo } ) => (
+  <div className='flex flex-col items-center justify-center gap-3 relative'>
+    <div className='aspect-1 h-40 -z-50 rounded-full mx-auto top-0 bg-slate-200 absolute' />
+    <img className='h-40 aspect-auto mt-2' src={photo} alt="Vehicle photo"/>
+    <h3>{name}</h3>
+    <Divisor quantity={5}/>
+    <p className='font-roboto text-xs text-gray-400'>{ description }</p>
+    <div className='flex items-center gap-3 text-gray-400'>
+      <div className='p-1 rounded-full border border-gray-400 cursor-pointer'>
+        <BsTwitter />
+      </div>
+      <div className='p-1 rounded-full border border-gray-400 cursor-pointer'>
+        <FaFacebookF />
+      </div>
+      <div className='p-1 rounded-full border border-gray-400 cursor-pointer'>
+        <AiOutlineInstagram />
+      </div>
+    </div> 
+  </div>  
 );
 
 export default VehicleCard;
