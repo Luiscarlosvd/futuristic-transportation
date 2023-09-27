bc67e00ef5a9023f43e9bf49f773fe9ed7a7300a
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-27 12:29:51 -0300

    Add responsive breakpoints for swiper

--- app/javascript/components/vehicles/VehiclesList.js
+++ app/javascript/components/vehicles/VehiclesList.js
@@ -3,13 +3,14 @@ import VehicleCard from './VehicleCard';
 import { TbTriangle } from 'react-icons/tb';
 import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Link } from 'react-router-dom'
 // import { PongSpinner } from 'react-spinners-kit';
 import 'swiper/css';
+import Divisor from './Divisor';
 
 const VehiclesList = () => {
   const swiperRef = useRef();
   const vehicles = useSelector((state) => state.vehicles);
-  const pagesQuantity = Math.ceil(7 / 3)
 
   const [listStart, setListStart] = useState(true)
   const [listEnd, setListEnd] = useState(false)
@@ -34,15 +35,19 @@ const VehiclesList = () => {
     }
   }
 
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <div className='flex w-screen'>
       <div className='navbar-space' />
       <div className='flex flex-col gap-10 min-h-screen vehicles-content justify-center bg-slate-50'>
         <div className='flex flex-col items-center gap-3 mx-auto'>
-          <h1 className='text-slate-700'>Latest Models</h1>
-          <p>Please select a Vehicle</p>
+          <h1 className='text-darkGrey text-4xl font-ace'>Latest Models</h1>
+          <p className='font-roboto text-gray-400 font-semibold text-sm'>Please select a Vehicle</p>
         </div>
-        <p className='text-center'>----------- divisor----------</p>
+        <Divisor quantity={15} />
         <div className='flex items-center justify-center w-full gap-8'>
           <div className={`slide mr-auto pl-8 pr-3 py-3 rounded-r-full bg-primaryGreen ${listStart ? "gray-bg" : ""}`}>
             <TbTriangle className="text-l text-white -rotate-90" onClick={() => {slideBack()}} />
@@ -53,18 +58,43 @@ const VehiclesList = () => {
               swiperRef.current = swiper;
             }}
             className='swiper-container'
-            spaceBetween={0}
             direction='horizontal'
-            slidesPerView={3}
-            allowTouchMove={false}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+                spaceBetween: 100,
+                allowTouchMove: true,
+              },
+              768: {
+                slidesPerView: 1,
+                spaceBetween: 100,
+                allowTouchMove: false,
+              },
+              890: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+                allowTouchMove: false,
+              },
+              1180: {
+                slidesPerView: 3,
+                spaceBetween: 15,
+                allowTouchMove: false,
+              },
+            }}
           >
-            <SwiperSlide><VehicleCard name='Vehicle1' description="Hola como estas" image="https://hola.com"/></SwiperSlide>
-            <SwiperSlide><VehicleCard name='Vehicle2' description="Hola como estas!" image="https://holis.com"/></SwiperSlide>
-            <SwiperSlide><VehicleCard name='Vehicle3' description="Hola como estas!!" image="https://holass.com"/></SwiperSlide>
-            <SwiperSlide><VehicleCard name='Vehicle3' description="Hola como estas!!" image="https://holass.com"/></SwiperSlide>
-            <SwiperSlide><VehicleCard name='Vehicle3' description="Hola como estas!!" image="https://holass.com"/></SwiperSlide>
-            <SwiperSlide><VehicleCard name='Vehicle3' description="Hola como estas!!" image="https://holass.com"/></SwiperSlide>
-            <SwiperSlide><VehicleCard name='Vehicle3' description="Hola como estas!!" image="https://holass.com"/></SwiperSlide>
+          {vehicles.vehicles.map((vehicle) => (
+            <SwiperSlide key={vehicle.id}>
+              <Link to={`/details/${vehicle.id}`} >
+            <VehicleCard
+              id={vehicle.id}
+              name={vehicle.name}
+              description={vehicle.description}
+              price={vehicle.price}
+              photo={vehicle.photo}
+            />
+            </Link>
+            </SwiperSlide>
+          ))}
       
           </Swiper>
         
