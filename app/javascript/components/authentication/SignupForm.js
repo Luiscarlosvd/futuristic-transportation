6f539707dd6b3099c07cbc7ab05063f0de6a78aa
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-28 21:03:18 -0400

    Add useForm hook and dispatch to createUser method
    Co-authored-by: alejuda <nachofino98@gmail.com>

--- app/javascript/components/authentication/SignupForm.js
+++ app/javascript/components/authentication/SignupForm.js
@@ -1,40 +1,11 @@
 import React, { useState } from 'react';
+import { createUser } from '../../redux/userSlice';
+import { useDispatch } from 'react-redux';
+import { useForm } from "react-hook-form";
 
 const SignupForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch('/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          user: { name: formData.name, email: formData.email, password: formData.password },
-        }),
-      });
-
-      if (response.ok) {
-        window.location.replace('/');
-      } else {
-        window.location.reload();
-      }
-    } catch (error) {
-      console.error('Error de red:', error);
-    }
-  };
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
 
   return (
     <div>
@@ -43,29 +14,29 @@ const SignupForm = () => {
           <div className="form-bg px-3 py-6 flex flex-col gap-16 items-center rounded-lg">
             <h1 className="font-ace text-2xl text-white text-shadow-title">Sign Up</h1>
 
-            <form className="flex flex-col gap-7 items-center" onSubmit={handleSubmit}>
+            <form 
+              className="flex flex-col gap-7 items-center"
+              onSubmit={handleSubmit((data) => dispatch(createUser( { user: data })))}
+            >
               <input
                 type="text"
-                placeholder="Nombre"
+                placeholder="Name"
                 name="name"
-                value={formData.name}
-                onChange={handleInputChange}
+                {...register("name")}
                 className="font-ace text-lg border-white rounded-full placeholder-white py-5"
               />
               <input
                 type="email"
-                placeholder="Correo Electrónico"
+                placeholder="Email"
                 name="email"
-                value={formData.email}
-                onChange={handleInputChange}
+                {...register("email")}
                 className="font-ace text-lg border-white rounded-full placeholder-white py-5"
               />
               <input
                 type="password"
-                placeholder="Contraseña"
+                placeholder="Password"
                 name="password"
-                value={formData.password}
-                onChange={handleInputChange}
+                {...register("password")}
                 className="font-ace text-lg border-white rounded-full placeholder-white py-5"
               />
               <button type="submit" className="bg-white font-ace text-slate-600 py-2 px-5 rounded-full transition-scale shadow-md">
