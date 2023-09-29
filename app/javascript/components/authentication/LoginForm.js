5fba52f728dafbed027d2c2173a84a3245eb25bb
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-28 21:04:04 -0400

    Add useForm hook and dispatch to loginUser method
    Co-authored-by: alejuda <nachofino98@gmail.com>

--- app/javascript/components/authentication/LoginForm.js
+++ app/javascript/components/authentication/LoginForm.js
@@ -1,59 +1,34 @@
 import React, { useState } from 'react';
+import { loginUser } from '../../redux/userSlice';
+import { useDispatch } from 'react-redux';
+import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
-  const [formData, setFormData] = useState({
-    login: '',
-    password: '',
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch('/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        window.location.replace('/');
-      } else {
-        window.location.reload();
-      }
-    } catch (error) {
-      console.error('Error de red:', error);
-    }
-  };
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
 
   return (
     <div className="w-full h-screen bg-registration">
       <div className="bg-image-form w-full h-screen grid place-content-center">
         <div className="form-bg px-3 py-6 flex flex-col gap-16 items-center rounded-lg">
           <h1 className="font-ace text-2xl text-white text-shadow-title">Log In</h1>
-          <form className="flex flex-col gap-7 items-center" onSubmit={handleSubmit}>
+          <form 
+            className="flex flex-col gap-7 items-center"
+            onSubmit={handleSubmit((data) => dispatch(loginUser(data)))}
+          >
             <input
               type="text"
               placeholder="Email or Username"
               name="login"
-              value={formData.login}
-              onChange={handleInputChange}
+              {...register("login")}
               className="font-ace text-lg border-white rounded-full placeholder-white py-5"
             />
             <input
               type="password"
               placeholder="Password"
               name="password"
-              value={formData.password}
-              onChange={handleInputChange}
+              {...register("password")}
               className="font-ace text-lg border-white rounded-full placeholder-white py-5"
             />
             <button
