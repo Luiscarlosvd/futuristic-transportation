81bd3d0d228559f0ab7a0900c40acdc73da5ca46
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-29 14:30:29 -0300

    Update Login & Signup forms styles
    Co-authored-by: luiscarlosvd <lc26cr7@gmail.com>

--- app/javascript/components/authentication/LoginForm.js
+++ app/javascript/components/authentication/LoginForm.js
@@ -11,10 +11,10 @@ const LoginForm = () => {
   return (
     <div className="w-full h-screen bg-registration">
       <div className="bg-image-form w-full h-screen grid place-content-center">
-        <div className="form-bg px-3 py-6 flex flex-col gap-16 items-center rounded-lg">
-          <h1 className="font-ace text-2xl text-white text-shadow-title">Log In</h1>
+        <div className="form-bg px-3 py-6 flex flex-col gap-10 items-center rounded-lg">
+          <h1 className="font-ace text-3xl text-white text-shadow-title">Log In</h1>
           <form 
-            className="flex flex-col gap-7 items-center"
+            className="flex flex-col gap-6 items-center"
             onSubmit={handleSubmit((data) => dispatch(loginUser(data)))}
           >
             <input
@@ -22,14 +22,14 @@ const LoginForm = () => {
               placeholder="Email or Username"
               name="login"
               {...register("login")}
-              className="font-ace text-lg border-white rounded-full placeholder-white py-5"
+              className="font-ace text-lg form-input placeholder-white py-3 px-5"
             />
             <input
               type="password"
               placeholder="Password"
               name="password"
               {...register("password")}
-              className="font-ace text-lg border-white rounded-full placeholder-white py-5"
+              className="font-ace text-lg form-input placeholder-white py-3 px-5"
             />
             <button
               type="submit"
@@ -39,8 +39,8 @@ const LoginForm = () => {
             </button>
           </form>
           <p className="text-white">
-            You don&apos;t have an account?
-            <Link to="/sign-up" className="underline text-blue-700"> Register</Link>
+            You don&apos;t have an account? {" "}
+            <Link to="/sign-up" className="underline text-primaryGreen">Register</Link>
           </p>
         </div>
       </div>
--- app/javascript/components/authentication/SignupForm.js
+++ app/javascript/components/authentication/SignupForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createUser } from '../../redux/userSlice';
 import { useDispatch } from 'react-redux';
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const SignupForm = () => {
   const dispatch = useDispatch();
@@ -11,8 +12,8 @@ const SignupForm = () => {
     <div>
       <div className="w-full h-screen bg-signup">
         <div className="bg-image-form w-full h-screen grid place-content-center">
-          <div className="form-bg px-3 py-6 flex flex-col gap-16 items-center rounded-lg">
-            <h1 className="font-ace text-2xl text-white text-shadow-title">Sign Up</h1>
+          <div className="form-bg px-3 py-6 flex flex-col gap-10 items-center rounded-lg">
+            <h1 className="font-ace text-3xl text-white text-shadow-title">Sign Up</h1>
 
             <form 
               className="flex flex-col gap-7 items-center"
@@ -23,27 +24,31 @@ const SignupForm = () => {
                 placeholder="Name"
                 name="name"
                 {...register("name")}
-                className="font-ace text-lg border-white rounded-full placeholder-white py-5"
+                className="font-ace text-lg form-input placeholder-white py-3 px-5"
               />
               <input
                 type="email"
                 placeholder="Email"
                 name="email"
                 {...register("email")}
-                className="font-ace text-lg border-white rounded-full placeholder-white py-5"
+                className="font-ace text-lg form-input placeholder-white py-3 px-5"
               />
               <input
                 type="password"
                 placeholder="Password"
                 name="password"
                 {...register("password")}
-                className="font-ace text-lg border-white rounded-full placeholder-white py-5"
+                className="font-ace text-lg form-input placeholder-white py-3 px-5"
               />
               <button type="submit" className="bg-white font-ace text-slate-600 py-2 px-5 rounded-full transition-scale shadow-md">
                 Register
               </button>
             </form>
-
+            <p className="text-white">
+            Go back to {" "}
+            <Link to="/log-in" className="underline text-primaryGreen">Login</Link>
+            {" "} page
+          </p>
           </div>
         </div>
       </div>
