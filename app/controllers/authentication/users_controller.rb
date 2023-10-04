bb3b096615284401eb0e6b810b94245f295f1095
mrcbq <marcobaquero@gmail.com>
2023-10-04 11:37:33 -0500

    Fix messages notice and alert at register page

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -5,7 +5,7 @@ class Authentication::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       session[:user_id] = @user.id
       redirect_to '/', notice: 'User Created!'
     else
