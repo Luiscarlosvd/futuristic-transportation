bb77c0586d1127370e2f829d267c4441a8068993
mrcbq <marcobaquero@gmail.com>
2023-10-04 15:27:41 -0500

    Change conf to deploy on render

--- config/puma.rb
+++ config/puma.rb
@@ -31,7 +31,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 # Please Install Linux on your computer.
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
